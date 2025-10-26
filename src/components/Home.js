661718309c097b5eedff9becfefc99505d1dc74d
Dan Mopsick <danmopsick@gmail.com>
2025-10-26 11:27:06 -0400

    Adding in the cards for the second sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import { makeStyles } from '@mui/styles';
-import { Link } from "react-scroll";
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 
 const useStyles = makeStyles(theme => ({
     movingArrow: {
@@ -51,40 +52,18 @@ export default function Home() {
             <div className="section">
                 <div id="landingPageImage" className='sectionWithBackgroundImage'>
                     <h1 className='heroText'>
-                    A fitness playground for the kid in all of us
+                        A fitness playground for the kid in all of us
                     </h1>
                 </div>
             </div>
 
             <div id="section2" className='longSection'>
-                <h1 className='sectionHeader'>Outpost Jersey Shore</h1>
-                <p className='sectionCaption'>
-                    Outpost Jersey Shore is a 24/7-365 Strength and Conditioning Facility located in Monmouth County.  With group and private coaching options for adults and athletes of all backgrounds, flexible memberships, and usage of the facility for outside trainers and other fitness professionals, Outpost is the Jersey Shore’s fitness and performance hub.
-                    Outpost is a community full of movers and doers supporting each other in their individual pursuit of every different goal that enters our facility. Our amazing collective of individuals creates a unique environment and family feel that will make Outpost your home away from home… hence the 24/7 access!
-                </p>
-
-
-                <p className='sectionCaption'>
-                    Packed into our garage gym set up is everything you need for a killer workout, including:
-                </p>
-                <ul className='infoList'>
-                    <li className='infoListItem'>3 Squat Racks</li>
-                    <li className='infoListItem'>2 Deadlift Platforms</li>
-                    <li className='infoListItem'>Nearly 30 yards of turf for carries, drags, and other athletic/functional movements</li>
-                    <li className='infoListItem'>Sleds, kettlebells, Farmer's Carry bars and other fun “odd object” implements to spice up a training routine</li>
-                    <li className='infoListItem'>A group of awesome people that make up the heart and soul of our space.</li>
-                </ul>
-
-                <Link to="training"
-                    smooth={true}
-                    duration={500}
-                    style={{
-                        textDecoration: "none",
-                        color: "white"
-                    }}
-                >
-                    <h1>JOIN <ArrowDownwardIcon className={classes.movingArrow} fontSize="large" /></h1>
-                </Link>
+                <Card sx={{ minWidth: 200 }}>
+                    <CardContent>
+                        <h1>Community</h1>
+                        <img src='/images/community1.png' className='roundedImage' />
+                    </CardContent>
+                </Card>
             </div>
 
             <div id="training" className='section'>
