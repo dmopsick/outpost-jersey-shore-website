6316119e8fd6d148921c399529ded8e6229fb4c3
Dan Mopsick <danmopsick@gmail.com>
2025-10-26 11:50:25 -0400

    Adding the card section

--- src/components/Home.js
+++ src/components/Home.js
@@ -57,40 +57,37 @@ export default function Home() {
                 </div>
             </div>
 
-            <div id="section2" className='longSection'>
-                <Card sx={{ minWidth: 200 }}>
+            <div id="section2"  className='bg-gray'>
+                <div className='cardContainer'>
+   <Card className='card' sx={{ minWidth: 300 }}>
                     <CardContent>
                         <h1>Community</h1>
                         <img src='/images/community1.png' className='roundedImage' />
                     </CardContent>
                 </Card>
+                
+
+                <Card className='card' sx={{ minWidth: 300 }}>
+                    <CardContent>
+                        <h1>Accessibility</h1>
+                        <img src='/images/accessibility1.png' className='roundedImage' />
+                    </CardContent>
+                </Card>
+
+                <Card className='card' sx={{ minWidth: 300 }}>
+                    <CardContent>
+                        <h1>Performance</h1>
+                        <img src='/images/performance1.png' className='roundedImage' />
+                    </CardContent>
+                </Card>
+                </div>
+             
+
+
             </div>
 
             <div id="training" className='section'>
-                <h1 className='sectionHeader'>Join our community</h1>
-                <p className='sectionCaption'>
-                    Need a little coaching guidance to get started? Tap in with Coach Shane and Coach Rob to get started with some coaching to take your game and your fitness journey to the next level - go to ‘Learn More’ to inquire about coaching
-                    <br />
-                    Otherwise get yourself rolling with our online sign-up - hit the link, fill out some basic info, and get started today!
-
-                </p>
-
-                <Button className='button'
-                    target="_blank"
-                    variant="contained"
-                    size="large"
-                    href="https://theoutpost.gymmasteronline.com/portal/signup">
-                    SIGN UP
-                </Button>
-
-                <Button className='button'
-                    target="_blank"
-
-                    variant="contained"
-                    size="large"
-                    href="https://forms.gle/ARkPTHWgBxu76Pny7">
-                    LEARN MORE
-                </Button>
+            
             </div>
         </div>
 
