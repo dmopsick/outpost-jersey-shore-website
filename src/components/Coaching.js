1deeae14b7b3009982f5e1ccae9540d69e83ae03
Dan Mopsick <danmopsick@gmail.com>
2026-09-01 20:34:17 -0400

    feat: Fall 2026 updates per Shane's request

--- src/components/Coaching.js
+++ src/components/Coaching.js
@@ -2,16 +2,31 @@ import Button from '@mui/material/Button';
 
 export default function Coaching() {
   return (
-    <div className='page-container black-background center-text section'>
-      <h1 className="section-header">Coaching</h1>
-      <p className='section-subheader'>COACHING INFO COMING SOON.</p>
-      <Button className='button'
-        variant="contained"
-        size="small"
-        target="_blank"
-        href="https://forms.gle/ARkPTHWgBxu76Pny7">
-                Learn More
-      </Button>
+    <div className='padding-bottom page-container black-background'>
+      <h1 className="section-header">Coaching hours</h1>
+      <ul className='info-list margin-top'>
+        <li>Monday: 7-10 am / 4-6 pm</li>
+        <li>Tuesday: 7-10 am</li>
+        <li>Wednesday: 7-10 am</li>
+        <li>Thursday: 8-10 am
+        </li>
+        <li>Friday: 7-10 am</li>
+      </ul>
+
+      <p className='large-body-text margin-top'>
+        For the times listed, please be in the gym 30 minutes before the end
+        time — so in by 9:30 am for a 10 am &quot;close&quot;.
+      </p>
+
+      <div className='center-text'>
+        <Button className='button'
+          variant="contained"
+          size="small"
+          target="_blank"
+          href="https://forms.gle/ARkPTHWgBxu76Pny7">
+                    Learn More
+        </Button>
+      </div>
     </div>
   );
 }
